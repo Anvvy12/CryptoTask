126e09695c5a32bbda093c2bc00c0cf29c589051
Bogdan <romanbs.wo@gmail.com>
2023-07-27 14:59:25 +0300

    s

--- src/app/wallet.gateway.js
+++ src/app/wallet.gateway.js
@@ -3,12 +3,8 @@ import axios from "axios";
 import { Web3Provider } from "@ethersproject/providers";
 import { Contract, ethers } from "ethers";
 
-// Функція для перетворення рядка шестнадцяткового числа у десяткове
-function convertHexToDecimal(hexValue) {
-  const decimalValue = new BigNumber(hexValue).toString(10);
-  const roundedValue = parseFloat(decimalValue);
-  return roundedValue;
-}
+const result = BigNumber(0.3).minus(0.1);
+console.log(result.c / 10 ** 14);
 
 async function connectToMetaMask() {
   if (window.ethereum) {
@@ -18,24 +14,23 @@ async function connectToMetaMask() {
       const signer = provider.getSigner();
       const walletAddress = await signer.getAddress();
       const balance = await provider.getBalance(walletAddress);
-      const tokenBalance = convertHexToDecimal(
-        ethers.formatEther(balance._hex)
-      );
+      const tokenBalance = ethers.formatEther(balance._hex);
+      const blockNumber = await provider.getBlockNumber();
+      console.log("blockNumber: ", blockNumber);
 
       try {
         const network = await provider.getNetwork();
         const networkAdres = network.ensAddress;
+        const networkName = network.name;
         const abi = await fetchDataABI(networkAdres).then(
           (data) => data.result
         );
         const contract = new Contract(networkAdres, abi, provider);
-        const contractName = contract;
-        const networktName = network;
 
-        const listAccounts = await provider.getBlock();
-        console.log("listAccounts: ", listAccounts);
-        console.log("networktName: ", networktName);
-        console.log("contractName: ", contractName);
+        const block = await provider.getBlock();
+        console.log("Block: ", block);
+        console.log("networkName: ", networkName);
+        console.log("contract: ", contract);
         console.log("networkAdres: ", networkAdres);
       } catch (error) {
         console.log("Error: ", error);
